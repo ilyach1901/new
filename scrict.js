dc8fcbbd4c0a9648c3dca4f8922978579a63212a
ilya <ilyachursin1901@gmail.com>
2020-01-26 21:32:17 +0500

    about new

---
+++ scrict.js
@@ -0,0 +1,36 @@
+alert('привет меня зовут siri')
+
+var userName = prompt('как вас зовут?')
+
+alert(userName + ' приятно с вами познакомиться! ' + userName + ' напишите мне два числа я сложу ')
+
+var num1 = prompt('введите первое число')
+var num2 = prompt('введите второе число')
+var summ = +num1 + +num2
+
+alert('сумма сложение двух чисел ровна = ' + summ )
+
+alert(userName + ' давайте еще раз! ' + userName + ' жду два числа я вычту ')
+
+var num1 = prompt('введите первое число')
+var num2 = prompt('введите второе число')
+var summ =-num1 - -num2
+
+alert('сумма вычитания двух чисел ровна = ' + summ )
+
+alert(userName + ' а еще раз! ' + userName + ' мне нужно два числа я умножу ')
+
+var num1 = prompt('введите первое число')
+var num2 = prompt('введите второе число')
+var summ =num1 * num2
+
+alert('сумма умножения двух чисел ровна = ' + summ )
+
+alert(userName + ' а ну еще разок! ' + userName + ' нужно два числа я разделю ')
+
+var num1 = prompt('введите первое число')
+var num2 = prompt('введите второе число')
+var summ =num1 / num2
+
+alert('сумма деления двух чисел ровна = ' + summ )
+
